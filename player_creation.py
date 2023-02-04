ccaf3746a792459b0d6a33f0bfc7e358915b68c5
unknown <28231539+lucaby@users.noreply.github.com>
2023-02-04 18:12:11 -0300

    started player creation prompt

---
+++ player_creation.py
@@ -0,0 +1,12 @@
+import utils
+
+def player_creation():
+    
+    """
+    
+    Prompts information for player creation
+
+    """
+
+    print("__________ Enter your Hero's name __________")
+    name = str(input("---> "))
