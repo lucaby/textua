922a9f6503d7bcb6e3c6049647d73e5e553771cc
unknown <28231539+lucaby@users.noreply.github.com>
2023-02-07 17:46:25 -0300

    improved player creation

--- player_creation.py
+++ player_creation.py
@@ -1,8 +1,20 @@
-import utils
+from utils import clear_window
 
-def player_creation():
+def player_creation_prompt():
     
     """ Prompts information for player creation """
 
-    print("__________ Enter your Hero's name __________")
-    name = str(input("---> "))
+    finished = False
+
+    while not finished:
+
+        clear_window()
+
+        print("==================================================")
+        print("\*/ \*/ \*/ \*/ Character Creation \*/ \*/ \*/ \*/")
+        print("==================================================")
+
+        print("_____________ Enter your Hero's name _____________")
+        name = str(input("---> "))
+
+        
