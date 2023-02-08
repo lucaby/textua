a64267c7195981aedf7d762b6e19f55174589cd2
unknown <28231539+lucaby@users.noreply.github.com>
2023-02-08 13:11:50 -0300

    add intro

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+from utils import clear_window
+
+def prompt_intro():
+
+    """ Prompts the intro text and info """
+
+    clear_window()
+    print(""" ▄▄▄· ·▄▄▄▄   ▌ ▐·▄▄▄ . ▐ ▄ ▄▄▄▄▄▄• ▄▌▄▄▄  ▄▄▄ .▄▄▄  .▄▄ ·      ▄ .▄      • ▌ ▄ ·. ▄▄▄ .
+             ▐█ ▀█ ██▪ ██ ▪█·█▌▀▄.▀·•█▌▐█•██  █▪██▌▀▄ █·▀▄.▀·▀▄ █·▐█ ▀.     ██▪▐█▪     ·██ ▐███▪▀▄.▀·
+             ▄█▀▀█ ▐█· ▐█▌▐█▐█•▐▀▀▪▄▐█▐▐▌ ▐█.▪█▌▐█▌▐▀▀▄ ▐▀▀▪▄▐▀▀▄ ▄▀▀▀█▄    ██▀▐█ ▄█▀▄ ▐█ ▌▐▌▐█·▐▀▀▪▄
+             ▐█ ▪▐▌██. ██  ███ ▐█▄▄▌██▐█▌ ▐█▌·▐█▄█▌▐█•█▌▐█▄▄▌▐█•█▌▐█▄▪▐█    ██▌▐▀▐█▌.▐▌██ ██▌▐█▌▐█▄▄▌
+              ▀  ▀ ▀▀▀▀▀• . ▀   ▀▀▀ ▀▀ █▪ ▀▀▀  ▀▀▀ .▀  ▀ ▀▀▀ .▀  ▀ ▀▀▀▀     ▀▀▀ · ▀█▄▀▪▀▀  █▪▀▀▀ ▀▀▀ """)
+    print("Dream fantasy home right in your terminal!")
+    print()
+    input("...Press any key to continue...")
+
 def main():
 
     """ Main program to start the game"""
