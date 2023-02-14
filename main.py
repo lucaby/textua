72dbf32209f2a5466506cf577bb9780b1b8eb649
unknown <28231539+lucaby@users.noreply.github.com>
2023-02-14 16:10:00 -0300

    working menu

--- main.py
+++ main.py
@@ -6,39 +6,57 @@ def intro():
 
     clear_window()
     print(""" ▄▄▄· ·▄▄▄▄   ▌ ▐·▄▄▄ . ▐ ▄ ▄▄▄▄▄▄• ▄▌▄▄▄  ▄▄▄ .▄▄▄  .▄▄ ·      ▄ .▄      • ▌ ▄ ·. ▄▄▄ .
-             ▐█ ▀█ ██▪ ██ ▪█·█▌▀▄.▀·•█▌▐█•██  █▪██▌▀▄ █·▀▄.▀·▀▄ █·▐█ ▀.     ██▪▐█▪     ·██ ▐███▪▀▄.▀·
-             ▄█▀▀█ ▐█· ▐█▌▐█▐█•▐▀▀▪▄▐█▐▐▌ ▐█.▪█▌▐█▌▐▀▀▄ ▐▀▀▪▄▐▀▀▄ ▄▀▀▀█▄    ██▀▐█ ▄█▀▄ ▐█ ▌▐▌▐█·▐▀▀▪▄
-             ▐█ ▪▐▌██. ██  ███ ▐█▄▄▌██▐█▌ ▐█▌·▐█▄█▌▐█•█▌▐█▄▄▌▐█•█▌▐█▄▪▐█    ██▌▐▀▐█▌.▐▌██ ██▌▐█▌▐█▄▄▌
-              ▀  ▀ ▀▀▀▀▀• . ▀   ▀▀▀ ▀▀ █▪ ▀▀▀  ▀▀▀ .▀  ▀ ▀▀▀ .▀  ▀ ▀▀▀▀     ▀▀▀ · ▀█▄▀▪▀▀  █▪▀▀▀ ▀▀▀ """)
-    print("Dream fantasy home right in your terminal!")
+         ▐█ ▀█ ██▪ ██ ▪█·█▌▀▄.▀·•█▌▐█•██  █▪██▌▀▄ █·▀▄.▀·▀▄ █·▐█ ▀.     ██▪▐█▪     ·██ ▐███▪▀▄.▀·
+         ▄█▀▀█ ▐█· ▐█▌▐█▐█•▐▀▀▪▄▐█▐▐▌ ▐█.▪█▌▐█▌▐▀▀▄ ▐▀▀▪▄▐▀▀▄ ▄▀▀▀█▄    ██▀▐█ ▄█▀▄ ▐█ ▌▐▌▐█·▐▀▀▪▄
+         ▐█ ▪▐▌██. ██  ███ ▐█▄▄▌██▐█▌ ▐█▌·▐█▄█▌▐█•█▌▐█▄▄▌▐█•█▌▐█▄▪▐█    ██▌▐▀▐█▌.▐▌██ ██▌▐█▌▐█▄▄▌
+          ▀  ▀ ▀▀▀▀▀• . ▀   ▀▀▀ ▀▀ █▪ ▀▀▀  ▀▀▀ .▀  ▀ ▀▀▀ .▀  ▀ ▀▀▀▀     ▀▀▀ · ▀█▄▀▪▀▀  █▪▀▀▀ ▀▀▀ """)
+    print("                         Dream fantasy home right in your terminal!")
     print()
     input("...Press any key to continue...")
 
 def menu():
     
     """ Menu functionality """
-    
-    clear_window()
-    print("Type the number of the option, then press the 'enter' key to select")
-    print("1. New game")
-    print("2. Load game")
-    print("3. Credits")
-    print("4. Quit")
 
-    choice = int(input("--->"))
+    def menu_text():
 
-    if choice == 1:
-        pass
-    elif choice == 2:
-        pass
-    elif choice == 3:
-        pass
-    elif choice == 4:
-        quit()
+        """ Prompts the menu text """
+
+        clear_window()
+        print("Type the number of the option, then press the 'enter' key to select")
+        print("1. New game")
+        print("2. Load game")
+        print("3. Credits")
+        print("4. Quit")
+
+    def menu_select():
+        
+        """ Handles menu choices """
+        
+        opt = str(input("---> "))
+
+        if opt == '1':
+            pass
+        elif opt == '2':
+            pass
+        elif opt == '3':
+            pass
+        elif opt == '4':
+            quit()
+        else:
+            menu_text()
+            menu_select()
+
+    # Displays menu
+    menu_text()
+    menu_select()
+
 
 def main():
 
     """ Main program to start the game"""
 
-    # intro()
-    # menu()
+    intro()
+    menu()
+
+main()
